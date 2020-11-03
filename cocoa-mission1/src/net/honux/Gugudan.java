01478de8957c5a16491dbce6f4dd0a4998ff840c
Hoyoung Jung <honux77@gmail.com>
2020-11-03 12:08:24 +0900

    Implement gogo - ok

--- cocoa-mission1/src/net/honux/Gugudan.java
+++ cocoa-mission1/src/net/honux/Gugudan.java
@@ -9,7 +9,18 @@ public class Gugudan {
         System.out.println("시작단을 입력해 주세요");
         int start = s.nextInt();
         System.out.println("끝단을 입력해 주세요");
+
         int end = s.nextInt();
         System.out.printf("%d %d\n", start, end);
+
+        //print start to end dan
+        for (int i = start; i <= end; i++) {
+            System.out.println(i + "Dan");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " * " + j + "=" + i * j);
+            }
+            System.out.println("=============\n");
+        }
+        s.close();
     }
 }
