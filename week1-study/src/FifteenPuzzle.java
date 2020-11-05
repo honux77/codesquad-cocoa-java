a7d090270d079187de93635c5728122aa9ad3114
Hoyoung Jung <honux77@gmail.com>
2020-11-05 18:01:53 +0900

    Refactor 15 puzzle using List and Collections.shuffle()

--- week1-study/src/FifteenPuzzle.java
+++ week1-study/src/FifteenPuzzle.java
@@ -1,40 +1,38 @@
-package net.honux;
+import java.util.Collections;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
 public class FifteenPuzzle {
     private final static int SIZE = 4;
     private int[][] board;
+
     public FifteenPuzzle() {
         this.board = new int[SIZE][SIZE];
-        int[] a = new int[SIZE * SIZE];
-        for(int i = 0; i < a.length; i++) {
-            a[i] = i;
+        List<Integer> numList = new ArrayList<Integer>();
+
+        for(int i = 0; i < SIZE * SIZE; i++) {
+            numList.add(i);
         }
-        shuffle(a);
+
+        Collections.shuffle(numList);
+
         for(int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                board[i][j] = a[i * SIZE + j];
+                board[i][j] = numList.get(i * SIZE + j);
             }
         }
     }
+
     public void printBoard() {
         for(int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                System.out.printf("%3d ", this.board[i][j]);
+                System.out.printf("%3d", this.board[i][j]);
             }
             System.out.println();
         }
     }
-    private void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-    private void shuffle(int[] a) {
-        Random r = new Random();
-        for (int i = a.length - 1; i > 0; i--) {
-            swap(a, i, r.nextInt(i));
-        }
-    }
+
     public static void main(String[] args) {
         FifteenPuzzle f = new FifteenPuzzle();
         f.printBoard();
