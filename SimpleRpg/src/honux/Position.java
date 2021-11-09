040a9ae6091bd2badeccd5464d3f8f99846fee34
Hoyoung Jung <honux77@gmail.com>
2021-11-09 16:37:57 +0900

    심플(하지 않은) RPG 초기화 일부 함
    한 것:
     - 게임 객체, 플레이어 객체 초기화
     - 몬스터 초기화
    
    못한 것:
     - 몬스터의 랜덤 자리 정하는 게 너무 어려웠다. ㅜㅜ.
     - 다음 시간의 나에게 넘기고 쉬자

---
+++ SimpleRpg/src/honux/Position.java
@@ -0,0 +1,27 @@
+package honux;
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
