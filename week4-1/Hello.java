70d4905806a54f770e6df1e641128d65bd1435b6
Hoyoung Jung <honux77@gmail.com>
2021-11-23 15:36:13 +0900

    버튼 이벤트 핸들러 익명 클래스로 구현

--- week4-1/Hello.java
+++ week4-1/Hello.java
@@ -12,9 +12,13 @@ public class Hello {
         f.setLayout(null);
 
         Button b = new Button("Jung");
-        
-        ButtonClickListener bcl = new ButtonClickListener();
-        b.addActionListener(bcl);
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("클릭 클릭");                
+            }
+        });
 
         b.setSize(100, 30);
         b.setLocation(100, 50);
